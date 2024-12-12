1194040396283fe95826b93b5e19fe6e26e7ca77
Migiro-Johans <Migironyogora@gmail.com>
2024-12-12 08:03:07 -0800

    simple interest recalculated

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,10 +1,10 @@
 def cal(principal, rate, time):
-    interest = (principal * rate * time)
+    interest = principal * rate * time
     return interest
 
-principal =1000
+principal = 1000
 rate = 0.05
-time =3 
+time = 3 
 
 interest = cal(1000, 0.05, 3)
 print(f"The simple interest is: {interest}")
