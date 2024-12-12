d5a90bf3fdb0c8a4ec1cf86c46d9e02a0a0579cb
Migiro-Johans <Migironyogora@gmail.com>
2024-12-12 07:39:17 -0800

    Future age calculator

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,6 +1,6 @@
-age = int(input("How old are you: "))
+age =input("How old are you? ")
 def cal(age):
-    future_age = (2050-2023) + age
+    future_age = (2050-2023) + int(age)
     return future_age
 future_age = cal(age)
 print(f"In 2050, you will be {future_age} years old")
