f8827e08a7721c3bd4847cdd4d2d0cceb15f037d
Migiro-Johans <Migironyogora@gmail.com>
2024-12-12 04:10:17 -0800

    changing hours to seconds

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,7 +1,7 @@
 hours = 2
-def cal (hour):
-    seconds = hour* 3600
+def cal (hours):
+    seconds = hours * 3600
     return seconds
 
-seconds = cal( 2)
+seconds = cal(2)
 print(f"{hours} is {seconds} seconds.")
