9a50fef893dc85e49e8ff87bb5e0e693cc6006ca
Migiro-Johans <Migironyogora@gmail.com>
2025-01-26 01:46:59 -0800

    a library management system to track issuance of books

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -20,7 +20,7 @@ class Book:
         return not self._is_checked_out
 class Library:
     def __init__(self):
-        self.books = []
+        self._books = []
     def add_book (self, book):
         if isinstance(book, Book):
             self._books.append(book)
