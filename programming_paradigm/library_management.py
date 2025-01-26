f326f5841655180e26ddaef6601b4b2da0635672
Migiro-Johans <Migironyogora@gmail.com>
2025-01-26 01:43:41 -0800

    a library management system to track issuance of books

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,53 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def check_out(self):
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
+
+    def return_book(self):
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False
+
+    def is_available(self):
+        return not self._is_checked_out
+class Library:
+    def __init__(self):
+        self.books = []
+    def add_book (self, book):
+        if isinstance(book, Book):
+            self._books.append(book)
+        else:
+            raise ValueError("Only instances of Book can be added to the library")
+
+    def check_out_book(self,title):
+        for book in self._boooks:
+            if book.title == title and book.is_available():
+                boook.check_out()
+                print(f"Checked out '{title}'.")
+                return
+        print(f"'{title}' is not available for checkout.")
+        
+    def return_book(self, title):
+        for book in self._books:
+            if book.title == title and not book.is_available():
+                book.return_book()
+                print(f"Returned '{title}'.")
+                return
+        print(f"'{title}' is not currently checked out.")
+
+    def list_available_books(self):
+        available_books = [book for book in self._books if book .is_available()]
+        if available_books:
+            for book in available_books:
+                print(f"{boo.title} by {book.author}")
+        else:
+            print("No books available")
+
